19f3e37fe256772a00c6ac2638c20ea95f275492
CarlosCalMen <carloscalmen11@gmail.com>
2025-02-04 10:53:09 -0500

    se agrega función actualizarListaAmigos

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 //  Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+const amigos = [];
 
 const agregarAmigo = ()=>{ 
     let amigo = document.getElementById('amigo').value; 
     if (amigo!=''){
         document.getElementById('amigo').value='';
-        console.log(listaAmigos);
-        return listaAmigos.push(amigo);
+        amigos.push(amigo);
+        console.log(amigos);
+        actualizarListaAmigos();
     }    
     else {
         alert('Error debe ingresar un nombre de amigo');
@@ -15,5 +16,16 @@ const agregarAmigo = ()=>{
     };
 };
 
+const actualizarListaAmigos = ()=>{
+    let listaAmigos= document.getElementById('listaAmigos');
+    listaAmigos.innerHTML='';
+    for (let i=0; i<amigos.length;i++) {  
+        let elementoLista = document.createElement('li');
+        elementoLista.innerHTML =amigos[i];
+        elementoLista.setAttribute('id',i); 
+        listaAmigos.appendChild(elementoLista);
+    };    
+    return;
+};
 
 
