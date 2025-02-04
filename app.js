9441d8db54c603228d56c974f5e117d259472cf4
CarlosCalMen <carloscalmen11@gmail.com>
2025-02-04 10:30:56 -0500

    Se actualiza función agregarAmigo

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 //  Aquí deberás desarrollar la lógica para resolver el problema.
-let nombres = Array;
+let listaAmigos = [];
+
+const agregarAmigo = ()=>{ 
+    let amigo = document.getElementById('amigo').value; 
+    if (amigo!=''){
+        document.getElementById('amigo').value='';
+        console.log(listaAmigos);
+        return listaAmigos.push(amigo);
+    }    
+    else {
+        alert('Error debe ingresar un nombre de amigo');
+        return;
+    };
+};
+
+
 
